dbc448eedc6fefa6b95e5fde23dad50271a4e36f
Oliver Lopez <oliverberto@gmail.com>
2023-10-11 14:14:38 -0400

    playing around with tailwind hero sections

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BeakerIcon } from '@heroicons/react/24/solid'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function MyComponent() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Appetizer Menu
-    </h1>
+    <div>
+      <BeakerIcon className="h-6 w-6 text-blue-600" />
+      <p>...</p>
+    </div>
   )
 }
-
-export default App
